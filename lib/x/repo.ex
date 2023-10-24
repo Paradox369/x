b96446a0d575df35eea53105d0af7344bfe73cbe
Paradox369 <george.ketcha@gmail.com>
2023-10-24 10:22:05 +0100

    reconfigured repo file

--- lib/x/repo.ex
+++ lib/x/repo.ex
@@ -1,5 +1,6 @@
 defmodule X.Repo do
-  use Ecto.Repo,
-    otp_app: :x,
-    adapter: Ecto.Adapters.Postgres
+  # use Ecto.Repo,
+  #   otp_app: :x,
+  #   adapter: Ecto.Adapters.Postgres
+  use AshPostgres.Repo, otp_app: :x
 end
